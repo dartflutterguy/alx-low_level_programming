d0134a4ffffb8819a888d69d8c2e440808232422
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-17 15:33:08 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,11 @@
 
 /* betty style doc for function main goes there */
 /**
- * This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n
-*/
+ * main - This program will assign a random
+ * number to the variable n each time it is
+ * executed
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
