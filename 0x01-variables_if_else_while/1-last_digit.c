a739bf93bab44baf3b2478f96f6bacdb3100b06a
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-17 16:31:35 -0800

    ammend

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,9 @@
 
 /* betty style doc for function main goes there */
 /**
-* main - prints the last digit of n and prints
-* out whether it is greater than 5, 0, or less than 6
-* Return: 0
+* main - This program will assign a random
+* number to the variable n
+* Return:0
 */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-		lastDigit = n % 10;
+	astDigit = n % 10;
 	printf("Last digit of %i is %i and is ", n, lastDigit);
 	if (lastDigit > 5)
 		printf("greater than 5\n");
@@ -26,4 +26,3 @@ int main(void)
 		printf("less than 6 and not 0\n");
 	return (0);
 }
-
