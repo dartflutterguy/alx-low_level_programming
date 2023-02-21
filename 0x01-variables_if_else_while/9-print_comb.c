d87dc1a26581460612a4e2cd5b9f8b8daf8760b1
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 02:17:27 -0800

    fixed 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,24 +17,24 @@
  */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < 100)
+	while (i < 100)
 	{
-        if (i / 10 < i % 10)
-        {
-        	putchar(i / 10 + '0');
-        	putchar(i % 10 + '0');
+	if (i / 10 < i % 10)
+	{
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
 
-        if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
-    }
+	if (i < 89)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	i++;
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
