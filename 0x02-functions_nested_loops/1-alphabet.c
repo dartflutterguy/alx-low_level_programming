bbe7e39a6fec6456d8d3eaaadc4c5e224b9f9e58
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 10:59:26 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * print_alphabet - Write a function that prints the alphabet,
  * in lowercase, followed by a new line
  *
  * Return: Always 0.
  */
-int main(void)
-{
-
-	print_alphabet();
-	return (0);
-
-}
-
 void print_alphabet(void)
 {
 
