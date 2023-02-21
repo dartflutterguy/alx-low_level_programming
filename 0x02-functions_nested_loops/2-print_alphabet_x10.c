a9d32b1bfa4ad61ff4f892093a722146cafd4270
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 12:34:18 -0800

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 1; j <= 10; j++)
+		_putchar(j);
+	}
+	_putchar(i);
+}
