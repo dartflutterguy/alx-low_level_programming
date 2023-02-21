a6f2dffe7d4cee0e2afe87746ef7620868ebc07c
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 09:28:35 -0800

    char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,16 @@
 
 int main(void)
 {
-	char c[] = "_putchar";
 
-	_putchar(char c);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
