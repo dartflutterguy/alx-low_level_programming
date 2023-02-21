2e73dc0d43bb24837932416e90f2ebefaac2f0e3
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 10:56:51 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * print_alphabet - Write a function that prints the alphabet,
+ * in lowercase, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+
+}
+
+void print_alphabet(void)
+{
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #include <unistd.h>
 
 char _putchar(char c);
+void print_alphabet(void);
