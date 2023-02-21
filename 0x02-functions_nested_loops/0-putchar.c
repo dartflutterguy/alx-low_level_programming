5f1e6d2f70dca38ef1fd1cac4165b5cabfeab170
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-21 08:04:18 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - a program that prints _putchar, followed by a new line
+* Return: 0
+*/
+
+int main(void)
+{
+	char *sh = "Holberton";
+
+	_putchar('\n');
+
+	return (0);
+}
