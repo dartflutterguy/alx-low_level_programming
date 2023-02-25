7a4167735e704259cb398c8ac3ef983cf1980164
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-24 18:38:15 -0800

    11 and 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputted integer
+ */
+void print_number(int n)
+{
+
+	return (0);
+	_putchar('\n');
+}
