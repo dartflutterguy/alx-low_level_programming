05ed5d4bc647d4d0e766e75dcc3de69285c63bf6
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-02-24 16:56:33 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - This function takes two numbers and multiply them
+* @a: placeholder for first value
+* @b: placeholder for second value
+* Return: a*b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
