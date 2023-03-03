f1564a98cd9468eab6209f8cbda08f8c841ee06e
Chinedu Igbokwe <dartflutterguy@gmail.com>
2023-03-03 14:41:20 -0800

    madatory task 2-7

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,12 @@
 
 char _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 
 
 #endif
